8e030acb79680eda73a90eab222f0720682be6e2
Marta Tsiruh <martatsiruch@gmail.com>
2022-01-29 11:47:08 +0100

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListContainer = document.querySelector('.gallery');
+//console.log(galleryListContainer);
+
+const galleryItemsMarkup = createElementGallery(galleryItems);
+//console.log(galleryItemsMarkup);
+
+//add string createGalleryItems in div.gallery
+galleryListContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+// Create image cards
+function createElementGallery() {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    }).join("");
+}
+
+let gallery = new SimpleLightbox('.gallery__item', {
+    captionsData : "alt",
+    captionDelay : 250,
+});
+
+galleryListContainer.addEventListener('click', clickOnGalleryImage);
+
+function clickOnGalleryImage(event) {
+    //скасування діі браузера
+    event.preventDefault();
+
+    if(!event.target.classList.contains("gallery__image")){
+        return;
+    }
+    gallery.on('show.simplelightbox');
+}
