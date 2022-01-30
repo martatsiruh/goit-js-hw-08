b4c2bd04fd29161adb1c8e6438b0344cb5de861b
Marta Tsiruh <martatsiruch@gmail.com>
2022-01-30 21:19:11 +0100

    add 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
 
+/**const feedbackFormEl = document.querySelector('.feedback-form');
+
+feedbackFormEl.addEventListener("submit", clickSubmitForm);
+
+// хранилище обновлялось не чаще чем раз в 500 миллисекунд
+const THROTTLE_WAIT_TIME = 500;
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+
+function clickSubmitForm(event) {
+    event.preventDefault();
+}
+*/
+
+const feedbackFormEl = document.querySelector('.feedback-form')
+const feedbackMessageEl = document.querySelector('.feedback-form textarea')
+
+feedbackFormEl.addEventListener('submit', onFormSubmit);
+feedbackMessageEl.addEventListener('input', onTextareaInput);
+
+function onTextareaInput(event) {
+    const message = event.currentTarget.value;
+
+    localStorage.setItem('feelback-msg', message);
+}
