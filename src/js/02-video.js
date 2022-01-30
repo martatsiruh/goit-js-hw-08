beaaf8235902a0d27b904d97e8d65264a0ec8e58
Marta Tsiruh <martatsiruch@gmail.com>
2022-01-30 18:51:31 +0100

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const THROTTLE_WAIT_TIME = 1000
+const STORAGE_KEY = "videoplayer-current-time";
+const iframeEl = document.querySelector("#vimeo-player");
+const player = new Player(iframeEl);
+
+player.on("timeupdate", throttle(getCurrentTime, THROTTLE_WAIT_TIME))
+
+function getCurrentTime(data) {
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+}
+
+const savedPlayerTimeForStorage = localStorage.getItem(STORAGE_KEY);
+
+if(savedPlayerTimeForStorage) {
+    const parsedSavedPlayerTime = JSON.parse(savedPlayerTimeForStorage) 
+    player.setCurrentTime(parsedSavedPlayerTime)
+}
