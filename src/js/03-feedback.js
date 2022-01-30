b4f8bcbdccb8d5bad6c3eaaea91a0c6de0539a52
Marta Tsiruh <martatsiruch@gmail.com>
2022-01-30 21:54:15 +0100

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,28 +1,43 @@
 import throttle from 'lodash.throttle';
 
-/**const feedbackFormEl = document.querySelector('.feedback-form');
+const feedbackFormEl = document.querySelector('.feedback-form');
 
 feedbackFormEl.addEventListener("submit", clickSubmitForm);
+
+
 
 // хранилище обновлялось не чаще чем раз в 500 миллисекунд
 const THROTTLE_WAIT_TIME = 500;
 const STORAGE_KEY = "feedback-form-state";
 const formData = {};
 
+populateTextarea();
+
+feedbackFormEl.addEventListener("input", throttle(onFormInput, THROTTLE_WAIT_TIME));
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+    let savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMessage) {
+        savedMessage = JSON.parse(savedInputs);
+
+        Object.entries(savedMessage).forEach(([name, value]) => {
+            formData[name] = value
+            feedbackFormEl[name].value = value
+        })
+    }
+}
+
 
 function clickSubmitForm(event) {
     event.preventDefault();
-}
-*/
 
-const feedbackFormEl = document.querySelector('.feedback-form')
-const feedbackMessageEl = document.querySelector('.feedback-form textarea')
-
-feedbackFormEl.addEventListener('submit', onFormSubmit);
-feedbackMessageEl.addEventListener('input', onTextareaInput);
-
-function onTextareaInput(event) {
-    const message = event.currentTarget.value;
-
-    localStorage.setItem('feelback-msg', message);
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY)
+    feedbackFormEl.reset()
 }
